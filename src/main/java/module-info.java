8d889d2657fb3298e787089d77069fb4b271ed9e
Peter Robinson <peter.robinson@jax.org>
2023-03-04 09:54:29 -0500

    cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
+@SuppressWarnings({ "requires-automatic", "requires-transitive-automatic" })
 module org.experimental_model.schema {
     requires transitive com.google.protobuf;
-    requires transitive org.phenopackets.schema;
-    exports org.jax.experimental.schema;
-    exports org.jax.experimental.schema.core;
+   // exports org.jax.experimental.schema;
+   // exports org.jax.experimental.schema.core;
 }
