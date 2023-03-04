24cb113801456736f58c90c63b382c06643cb269
pnrobinson <peter.robinson@jax.org>
2023-03-04 15:26:41 +0000

    Deploying to gh-pages from @ TheJacksonLaboratory/ExperimentalModelSchema@65e78cad95e31b7ae4539d5e639dc97f93a80933 🚀

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.experimental_model.schema {
-    requires transitive com.google.protobuf;
-    requires transitive org.phenopackets.schema;
-    exports org.jax.experimental.schema;
-    exports org.jax.experimental.schema.core;
-}
