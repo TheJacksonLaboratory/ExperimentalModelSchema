8ae8fc7340a7e6e51dfe11219eadf458d7ecf9ad
Peter Robinson <peter.robinson@jax.org>
2023-03-05 12:51:45 -0500

    adding example

--- docs/index.rst
+++ docs/index.rst
@@ -2,11 +2,28 @@
 Welcome to the documentation for the experimental-model-schema!
 ###############################################################
 
-The goal of the experimental-model-schema (EMS)
-is to define a model for the description of genetic,
-genomic, experimental, and phenotypic information
-about an experiment with a model organism.
-//
+
+The Experimental Model Schema (EMS) is a semantic framework for
+modeling the phenotypic data of model organism experimental data. The
+EMS comprises information about the experimental conditions, genetic modifications
+of the model, phenotypic observations and measurements. The EMS is
+designed to be compatible with the
+`GA4GH Phenopacket Schema <https://phenopacket-schema.readthedocs.io/en/latest/index.html>`_.
+
+A goal of the EMS is to
+represent experimental outcomes across the entirety of the
+`Mouse Phenome Database <https://pubmed.ncbi.nlm.nih.gov/36330959/>`_ (MPD)
+and to represent both individuals or experimental cohorts. The EMS is designed
+to be flexible enough to represent experimental model data of other species.
+
+
+Nomenclature
+^^^^^^^^^^^^
+
+We refer to the model as the *Experimental Model Schema*. We refer to
+each instance of the EMS used to describe an experiment as a
+*model packet* (lower case).
+
 The schema as well as source code in Java, C++, and Python
 is available from the `experimental-model-schema GitHub repository <https://github.com/TheJacksonLaboratory/ExperimentalModelSchema>`_.
 
@@ -15,7 +32,8 @@ is available from the `experimental-model-schema GitHub repository <https://gith
    :caption: Contents
 
    introduction
-   emschema
+   experimental_model
+   experimental_cohort
    building-blocks
    #recommended-ontologies
    #working
---
+++ src/test/java/org/experimental/schema/ExperimentalModelTest.java
@@ -0,0 +1,147 @@
+package org.experimental.schema;
+
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import org.experimental.schema.core.Animal;
+import org.experimental.schema.core.Evidence;
+import org.experimental.schema.core.ExternalReference;
+import org.experimental.schema.core.ModelGenotype;
+import org.experimental.schema.core.OntologyClass;
+import org.experimental.schema.core.PhenotypicFeature;
+import org.experimental.schema.core.Sex;
+import org.experimental.schema.core.Study;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Model this data
+ * Allelic Composition	Allele Symbol(s)	Genetic Background	Mammalian Phenotype ID	PubMed ID	MGI Marker Accession ID (comma-delimited)
+ * Tg(Myh6-Nox4)1Ams/0	Tg(Myh6-Nox4)1Ams	B6.Cg-Tg(Myh6-Nox4)1Ams	MP:0001544	20921387	MGI:4838550
+ * Tg(Myh6-Nox4)1Ams/0	Tg(Myh6-Nox4)1Ams	B6.Cg-Tg(Myh6-Nox4)1Ams	MP:0002833	20921387	MGI:4838550
+ * Tg(Myh6-Nox4)1Ams/0	Tg(Myh6-Nox4)1Ams	B6.Cg-Tg(Myh6-Nox4)1Ams	MP:0003915	20921387	MGI:4838550
+ * Tg(Myh6-Nox4)1Ams/0	Tg(Myh6-Nox4)1Ams	B6.Cg-Tg(Myh6-Nox4)1Ams	MP:0004486	20921387	MGI:4838550
+ * Tg(Myh6-Nox4)1Ams/0	Tg(Myh6-Nox4)1Ams	B6.Cg-Tg(Myh6-Nox4)1Ams	MP:0010724	20921387	MGI:4838550
+ */
+public class ExperimentalModelTest {
+
+
+    private static org.experimental.schema.ExperimentalModel model;
+
+    private static ModelGenotype modelGenotype() {
+        // Tg(Myh6-Nox4)1Ams/0
+        //Nox4
+        OntologyClass nox4 = OntologyClass.newBuilder()
+                .setId("MGI:1354184")
+                .setLabel("Nox4")
+                .build();
+        ModelGenotype genotype = ModelGenotype.newBuilder()
+                .setGene(nox4)
+                .setGenotypeId("MGI:4838552")
+                .setGenotypedescription("Tg(Myh6-Nox4)1Ams")
+                .setGenotypesymbol("Tg(Myh6-Nox4)1Ams/0" ) // our model appears to be heterozygouys
+                .build();
+        return genotype;
+    }
+
+    private static Animal animal() {
+        /*
+  string animal_id = 1;
+  string animal_project_id = 2;
+  string project_symbol = 3;
+  Sex sex = 4;
+  int32 stock_number = 5;
+  OntologyClass taxonomy = 6;
+  string strain = 7;
+  string strain_identifier = 8;
+         */
+        OntologyClass mmusculus = OntologyClass.newBuilder()
+                .setId("NCBITaxon:10090")
+                .setLabel("Mus musculus")
+                .build();
+        Animal animal = Animal.newBuilder()
+                .setTaxonomy(mmusculus)
+                .setSex(Sex.UNKNOWN_SEX)
+                .setStrain("B6.Cg-Tg(Myh6-Nox4)1Ams")
+                .setStrainIdentifier("MGI:4839003") // This is a string in MPD but a CURIE here -- UNIFY!
+                .build();
+        return animal;
+    }
+
+
+    private static PhenotypicFeature phenotypicFeature(String id, String label) {
+
+        ExternalReference externalReference = ExternalReference.newBuilder()
+                .setId("PMID:20921387")
+                .setReference("https://pubmed.ncbi.nlm.nih.gov/20921387/")
+                .setDescription("NADPH oxidase-4 mediates protection against chronic load-induced stress in mouse hearts by enhancing angiogenesis")
+                .build();
+        OntologyClass evidenceCode = OntologyClass.newBuilder()
+                .setId("ECO:0000033")
+                .setLabel("author statement supported by traceable reference")
+                .build();
+        Evidence evidence = Evidence.newBuilder()
+                .setEvidenceCode(evidenceCode)
+                .setReference(externalReference)
+                .build();
+        OntologyClass clz = OntologyClass.newBuilder()
+                .setId(id)
+                .setLabel(label)
+                .build();
+        PhenotypicFeature pf = PhenotypicFeature.newBuilder()
+                .setType(clz)
+                .addEvidence(evidence)
+                .build();
+        return pf;
+    }
+
+    private static List<org.experimental.schema.core.PhenotypicFeature> phenotypicFeatureList() {
+        List<PhenotypicFeature> features = new ArrayList<>();
+        features.add(phenotypicFeature("MP:0001544", "abnormal cardiovascular system physiology"));
+        features.add(phenotypicFeature("MP:0002833", "increased heart weight"));
+        features.add(phenotypicFeature("MP:0003915", "increased left ventricle weight"));
+        features.add(phenotypicFeature("MP:0004486", "decreased response of heart to induced stress"));
+        features.add(phenotypicFeature("MP:0010724", "thick interventricular septum"));
+        return features;
+    }
+
+    private static Study study() {
+        Study study = Study.newBuilder()
+                .setId("study.ID")
+                .build();
+        return study;
+    }
+
+
+    @BeforeAll
+    public static void init() {
+        String id ="where do we get this model id?";
+        model = org.experimental.schema.ExperimentalModel.newBuilder()
+                .setId(id)
+                .setStudy(study())
+                .setAnimal(animal())
+                .addAllPhenotypicFeatureList(phenotypicFeatureList())
+                .addGenotypeList(modelGenotype())
+                .build();
+
+    }
+
+    @Test
+    public void testConvertToJson() throws InvalidProtocolBufferException {
+        String json = JsonFormat.printer().print(model);
+        System.out.println(json);
+        org.experimental.schema.ExperimentalModel.Builder animalBuilder = org.experimental.schema.ExperimentalModel.newBuilder();
+        JsonFormat.parser().merge(json, animalBuilder);
+        org.experimental.schema.ExperimentalModel fromJson = animalBuilder.build();
+        //Ta-da!
+        assertNotNull(json);
+
+    }
+
+
+}
