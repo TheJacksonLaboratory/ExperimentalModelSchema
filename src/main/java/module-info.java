7b29fbb4ff68231108921cd2a618f043f541f3fc
hansenp <peter.hansen@jax.org>
2023-07-20 15:27:48 +0200

    Added auto increment of test version number.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 @SuppressWarnings({ "requires-automatic", "requires-transitive-automatic" })
 module org.experimental.schema {
-    requires transitive com.google.protobuf;
+   requires transitive com.google.protobuf;
+
    exports org.experimental.schema;
    exports org.experimental.schema.core;
+
+   //exports org.experimental.schema.v2;
+   //exports org.experimental.schema.v2.core;
 }
